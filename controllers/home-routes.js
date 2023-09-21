e92a304efd6906d45b7541f05ea90e25f7b467af
Daniel Bonn <danieltbonn@gmail.com>
2023-09-20 20:56:10 -0500

    trying to view dummy product data on pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,7 +6,17 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
   try {
 
-    res.render('homepage');
+
+    const products = 
+      {
+        name: "Example Product Name",
+        price: 69420
+      };
+      console.log(products)
+
+    res.render('homepage',
+    products
+);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
