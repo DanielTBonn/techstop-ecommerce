a9a1ed293c651c1b811abe875848289a09f1ae61
Daniel Bonn <danieltbonn@gmail.com>
2023-09-28 17:43:14 -0500

    seed data

--- seeds/review-seeds.js
+++ seeds/review-seeds.js
@@ -23,18 +23,47 @@ const reviewData = [
         product_id: 1
     },
     {
-        id: 3,
+        id: 4,
         content: `This TV isn't big enough`,
         username: 'chrisD',
         user_id: 6,
         product_id: 35
     },
     {
-        id: 3,
-        content: `This TV isn't big enough`,
+        content: `This is crazy useful, thanks Belkin!`,
         username: 'chrisD',
         user_id: 6,
-        product_id: 35
+        product_id: 20
+    },
+    {
+        content: `I use this all the time`,
+        username: 'chrisD',
+        user_id: 6,
+        product_id: 53
+    },
+    {
+        content: `I couldn't ask for a more useful USB C hub! It even works with 4k!`,
+        username: 'ericV',
+        user_id: 4,
+        product_id: 53
+    },
+    {
+        content: `Best docker on the market!`,
+        username: 'abdulAl',
+        user_id: 5,
+        product_id: 53
+    },
+    {
+        content: `RGB on this is way too cool!`,
+        username: 'abdulAl',
+        user_id: 5,
+        product_id: 54
+    },
+    {
+        content: `Light up keys go brr.`,
+        username: 'DanielTBonn',
+        user_id: 1,
+        product_id: 54
     },
 ]
 
